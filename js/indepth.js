0c29a12f1fdc976ffd6f93b4ee87f69f37afa3c3
Rodrigo Luis Santander <roidersp@gmail.com>
2015-10-26 21:01:44 -0600

    dsfdsfssdf

--- js/indepth.js
+++ js/indepth.js
@@ -3,8 +3,9 @@ var ventana_ancho = $(window).width();
 var input_active = false;
 var start_location;
 var searchBox;
-var segundos;
-
+var segundos, bici_segundos, bici_segundos;
+var myVideo = document.getElementById("video"); 
+var vel_t= 300;
 
 $(document).on("click", "#indepth_share_twiiter", function(){
 	var text = encodeURIComponent("Hice "+segundos+" para llegar al Autódromo Hermanos Rodríguez en un Fórmula ¿Y tú?");
@@ -23,7 +24,6 @@ $(document).on("click", "#indepth_share_fb", function(){
 
 $(document).on("click","#boton_empezar",function(){
 	$("#indepth_pov").addClass("pov_active");	
-	$("#indepth_velocimetro").fadeIn();
 	$("#map_container").fadeOut("slow");
 	$("#cont_to_vel").fadeIn();
 });
@@ -35,7 +35,6 @@ function initAutocomplete() {
     
     searchBox.addListener('places_changed', function() {
 	 	input_active = true;
-	 	console.log("test");
  	});
  	
 }
@@ -70,29 +69,69 @@ $(document).on("click","#indepth_ver",function(){
 		  
 		 $(document).on("click","#boton_to_vel",function(){
 		
+		
+		 	$("#indepth_semaforo").fadeIn("fast");
+		 	
+		 	setTimeout(
+			  function() 
+			  {
+				$("#indepth_semaforo").fadeOut("fast");
+		 	
+		
 			$("#cont_to_vel").hide();
 			
 			$("#indepth_video_a").fadeIn("fast");
+			myVideo.play();
 			
+			num_vel = 76;
+			direcion = true;
+			
+			valocimetro_interval = setInterval(function(){ 
+				
+				if(direcion){
+					num_vel++;
+					
+					if(num_vel>=99){
+						direcion=false;
+					}
+						
+				}else{
+					num_vel--;
+					
+					if(num_vel<=76){
+						direcion=true;
+					}
+					
+				}
+				
+				$("#indepth_velocimetro").css("background-image",'url('+urlIndepth+'images/velocimetro/velocimetro_000'+num_vel+'.png)')
+								
+			}, 40);
+			
+			$("#indepth_velocimetro").fadeIn();
+						
 			setTimeout(
 			  function() 
 			  {
+				 myVideo.pause();
 			    $("#indepth_video_a").fadeOut("500");
 			    $("#indepth_resultados").show();
 			    $("#boton_de_nuevo").show();
-			  }, 3500);
+			    $("#indepth_velocimetro").fadeOut();
+			    clearInterval(valocimetro_interval);
+			  }, 4100);
 			
-			
-				panorama.setPosition({lat: 19.40319, lng: -99.09094});
-				panorama.setZoom(0);
-				panorama.setPov({
-			          heading: -70,
-			          pitch: 0
-			        });
+			panorama.setPosition({lat: 19.40319, lng: -99.09094});
+			panorama.setZoom(0);
+			panorama.setPov({
+	          heading: -70,
+	          pitch: 0
+	        });
+	        
+	        }, 2200);
 		
-	
+			
 		});
-		
 		
 		$(document).on("click","#boton_de_nuevo",function(){
 		avascript:location.reload();
@@ -107,7 +146,9 @@ $(document).on("click","#indepth_ver",function(){
    var myOptions = {
      zoom:8,
      mapTypeId: google.maps.MapTypeId.ROADMAP,
-     disableDefaultUI: true
+     disableDefaultUI: true,
+     durationInTraffic: true,
+ 
    }
 
    var map = new google.maps.Map(document.getElementById("map"), myOptions);
@@ -119,7 +160,6 @@ $(document).on("click","#indepth_ver",function(){
        origin: $("#pac-input").val(), 
        destination: new google.maps.LatLng(19.40319,-99.09094),
        travelMode: google.maps.DirectionsTravelMode.DRIVING,
-       
    };
 
    directionsService.route(request, function(response, status) {
@@ -129,22 +169,128 @@ $(document).on("click","#indepth_ver",function(){
 	  		metros = response.routes[0].legs[0].distance.value; 
 	  		segundos = response.routes[0].legs[0].duration.value;
 	  		
+	  		velocidad=(vel_t*1000)/3600;
 	  		
+	  		console.log(metros);
 	  		
 	  		$(".indepth_map_inicio").html($("#pac-input").val());
 	  		$("#indepth_kilometros").html(Math.round10(metros/1000, -2) + " km");
-	  		$("#res_distancia span").html(Math.round10(metros/1000, -2) + " km");
-	  		$("#res_tiempo_normal span").html(Math.round10(segundos,0) + " seg");
-	  		$("#res_tiempo span").html(Math.round10(metros/83.3333,0) + " seg");
+	  		$("#res_distancia div").html(Math.round10(metros/1000, -2) + " km");
+	  		$("#res_tiempo div").html((Math.round10(metros/velocidad)) + " segs");
+	  		
+	  		s3= metros/velocidad;
+	  		
+	  		if((s3/60)>1000){
+		  		$("#res_tiempo div").html(Math.round10((s3/3660),-1) + " HRS");
+	  		}else{
+	  		if(segundos>1000){
+		  		$("#res_tiempo div").html(Math.round10((s3/60),-1) + " MINS");
+	  		}else{
+		  		$("#res_tiempo div").html(s3 + " SEGS");
+	  		}
+	  		}
+	  		
+	  		
+	  		if((segundos/60)>1000){
+		  		$("#res_tiempo_carro").html(Math.round10((segundos/3660),-1) + " HRS");
+	  		}else{
+	  		if(segundos>1000){
+		  		$("#res_tiempo_carro").html(Math.round10((segundos/60),-1) + " MINS");
+	  		}else{
+		  		$("#res_tiempo_carro").html(segundos + " SEGS");
+	  		}
+	  		}
            start_location = response.routes[0].legs[0];
-           
            
            var st_view = $("#indepth_street_view");
 
          directionsDisplay.setDirections(response);
+      }else{
+	      $("#res_tiempo_carro").hide();
       }
    });
-		
+   
+   
+   var request = {
+       origin: $("#pac-input").val(), 
+       destination: new google.maps.LatLng(19.40319,-99.09094),
+       travelMode: google.maps.DirectionsTravelMode.TRANSIT,
+   };
+
+   directionsService.route(request, function(response, status) {
+	   
+     if (status == google.maps.DirectionsStatus.OK) {
+	  		segundos = response.routes[0].legs[0].duration.value;
+	  		
+	  		if((segundos/60)>1000){
+		  		$("#res_tiempo_bus").html(Math.round10((segundos/3660),-1) + " HRS");
+	  		}else{
+	  		
+	  		if(segundos>1000){
+		  		$("#res_tiempo_bus").html(Math.round10((segundos/60),-1) + " MINS");
+	  		}else{
+		  		$("#res_tiempo_bus").html(segundos + " SEGS");
+	  		}
+	  		}
+	  		
+	  		
+      }else{
+	      $("#res_tiempo_bus").hide();
+      }
+   });
+   
+
+   
+   var request = {
+       origin: $("#pac-input").val(), 
+       destination: new google.maps.LatLng(19.40319,-99.09094),
+       travelMode: google.maps.DirectionsTravelMode.WALKING,
+   };
+
+   directionsService.route(request, function(response, status) {
+	   
+      if (status == google.maps.DirectionsStatus.OK) {
+	  		segundos = response.routes[0].legs[0].duration.value;
+	  		if((segundos/60)>1000){
+		  		$("#res_tiempo_walk").html(Math.round10((segundos/3660),-1) + " HRS");
+	  		}else{
+	  		if(segundos>1000){
+		  		$("#res_tiempo_walk").html(Math.round10((segundos/60),-1) + " MINS");
+	  		}else{
+		  		$("#res_tiempo_walk").html(segundos + " SEGS");
+	  		}
+	  		}
+	  		
+      }else{
+	      $("#res_tiempo_walk").hide()
+      }
+   });
+   
+    var request = {
+       origin: $("#pac-input").val(), 
+       destination: new google.maps.LatLng(19.40319,-99.09094),
+       travelMode: google.maps.DirectionsTravelMode.BICYCLING,
+   };
+
+   directionsService.route(request, function(response, status) {
+	   
+	   console.log(response);
+	   
+      if (status == google.maps.DirectionsStatus.OK) {
+	  	segundos = response.routes[0].legs[0].duration.value;
+	  	if((segundos/60)>1000){
+		  		$("#res_tiempo_bici").html(Math.round10((segundos/3660),-1) + " MINS");
+	  		}else{
+		  	if(segundos>1000){
+		  		$("#res_tiempo_bici").html(Math.round10((segundos/60),-1) + " MINS");
+	  		}else{
+		  		$("#res_tiempo_bici").html(segundos + " SEGS");
+	  		}
+	  		}
+      }else{
+	    $("#res_tiempo_bici").hide();  
+      }
+   });
 		
 	}
 
@@ -199,7 +345,6 @@ function bgadj(){
                 
                 video.setAttribute("style", "left:"+ajuste+"px");          
             }
-          
         }
         else{ 
           
